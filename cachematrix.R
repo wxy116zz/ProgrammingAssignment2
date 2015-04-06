9f081408f8df87407f2c2bbdac472d78078dde1d
wxy116zz <wxy116zz@users.noreply.github.com>
2015-04-06 17:43:38 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+sf
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
